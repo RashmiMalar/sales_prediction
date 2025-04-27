bac6297bf9eeb534c754e3c01db7fb184ff1f8e8
RashmiMalar <rashmimalarg@gmail.com>
2025-04-28 00:54:24 +0530

    Updated project: Added requirements.txt and made necessary changes

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import io
 import base64
+import os
 
 app = Flask(__name__)
 
@@ -67,5 +68,7 @@ def predict():
     except Exception as e:
         return render_template('index.html', status_message=f"Error during prediction: {str(e)}")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
+
+
